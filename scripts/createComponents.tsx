ec108b468ef16415297440480879cb7f3c969496
adamkong256@gmail.com <adamkong256@gmail.com>
2023-11-09 16:16:07 -0700

    + added createComponent script

---
+++ scripts/createComponents.tsx
@@ -0,0 +1,91 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+// Retrieve command-line arguments
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Please provide a file name as an argument.");
+  process.exit(1);
+}
+
+// Extract the provided file name
+const folderName = args[0];
+const componentName = args[0];
+const fileExtension = "tsx"; // You can customize the file extension here
+
+// Define the directory where you want to create files
+const targetDirectory = path.resolve(__dirname, "../components", folderName);
+
+// Create the target directory if it doesn't exist
+fs.ensureDirSync(targetDirectory);
+
+// Create the component file inside the folder
+const ComponentFilePath = path.join(
+  targetDirectory,
+  `${componentName}.${fileExtension}`
+);
+fs.writeFileSync(
+  ComponentFilePath,
+  `
+  import {I${componentName}} from "./Type";
+  import styled from "styled-components";
+
+const Styled${componentName} = styled.div<I${componentName}>\`\`;
+
+const ${componentName} = ({}: I${componentName}) => {
+  return <Styled${componentName}></Styled${componentName}>;
+}
+
+export default ${componentName};
+`.trim()
+);
+
+// Create the Type file inside the folder
+const TypeFilePath = path.join(targetDirectory, `Type.${fileExtension}`);
+
+fs.writeFileSync(
+  TypeFilePath,
+  `
+export interface I${componentName} {}
+`.trim()
+);
+
+// Create the story file inside the folder
+const StoryFilePath = path.join(targetDirectory, `${componentName}.story.ts`);
+fs.writeFileSync(
+  StoryFilePath,
+  `
+import type { Meta, StoryObj } from "@storybook/react";
+import ${componentName} from "./${componentName}";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "Components/${componentName}",
+  component: ${componentName},
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof ${componentName}>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+};
+`.trim()
+);
+
+// Create the index.ts file inside the folder
+
+const IndexFilePath = path.join(targetDirectory, "index.ts");
+fs.writeFileSync(
+  IndexFilePath,
+  `
+export * from "./${componentName}";
+export * from "./Type";
+`.trim()
+);
